624141b1247c09d4ab14657561ebebe35590c619
zyhou <richard.maxime9@gmail.com>
2017-03-28 10:23:06 +0200

    CS

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,20 +3,18 @@ import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import App from '../pages/index';
 
-describe('Menu Layout', () => {
-    it('Title show Awale', () => {
+describe('Title', () => {
+    it('should contain the word "Awale"', () => {
         const app = shallow(<App />);
         expect(app.find('h1').text()).toEqual('Awale');
     });
 
-    it('Have 2 links on menu', () => {
+    it('should contain two links', () => {
         const app = shallow(<App />);
         expect(app.find('a').length).toEqual(2);
     });
-});
 
-describe('Menu Layout with Snapshot Testing', () => {
-    it('App shows Menu layout', () => {
+    it('should match with snapshot', () => {
         const component = renderer.create(<App />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
