540ad424b4d3180902646f93b921ef19d1088fea
zyhou <richard.maxime9@gmail.com>
2017-04-03 15:01:32 +0200

    correction test index.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
-import App from '../pages/index';
+import { Index } from '../pages/index';
 
 describe('Title', () => {
     it('should contain the word "Awale"', () => {
-        const app = shallow(<App />);
+        const app = shallow(<Index />);
         expect(app.find('h1').text()).toEqual('Awale');
     });
 
     it('should contain two element in the menu', () => {
-        const app = shallow(<App />);
+        const app = shallow(<Index />);
         expect(app.find('a').length).toEqual(2);
     });
 
     it('should match with snapshot', () => {
-        const component = renderer.create(<App />);
+        const component = renderer.create(<Index />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
     });
