e35081b0d0df320ad1642459f468b5af822d4e8e
zyhou <richard.maxime9@gmail.com>
2017-04-05 10:12:37 +0200

    correct test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,17 +5,17 @@ import { Index } from '../pages/index';
 
 describe('Title', () => {
     it('should contain the word "Awale"', () => {
-        const app = shallow(<Index />);
+        const app = shallow(<Index startGame={() => {}} />);
         expect(app.find('h1').text()).toEqual('Awale');
     });
 
     it('should contain two element in the menu', () => {
-        const app = shallow(<Index />);
+        const app = shallow(<Index startGame={() => {}} />);
         expect(app.find('a').length).toEqual(2);
     });
 
     it('should match with snapshot', () => {
-        const component = renderer.create(<Index />);
+        const component = renderer.create(<Index startGame={() => {}} />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
     });
