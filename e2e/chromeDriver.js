b2961da896f0c0ca1bdee026136d7ae214a1ecb7
zyhou <richard.maxime9@gmail.com>
2017-04-07 10:11:48 +0200

    use path join chrome driver

--- e2e/chromeDriver.js
+++ e2e/chromeDriver.js
@@ -1,7 +1,8 @@
+const path = require('path');
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
-const chromePath = `${__dirname}/../${process.env.SELENIUM_BROWSER_BINARY_PATH}`;
+const chromePath = path.join(__dirname, '..', process.env.SELENIUM_BROWSER_BINARY_PATH);
 const service = new chrome.ServiceBuilder(chromePath).build();
 
 chrome.setDefaultService(service);
